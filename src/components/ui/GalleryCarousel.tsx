be899fbf875323d4759489dec49e20a2b525b442
Boyko Lalov <boiskila@gmail.com>
2026-01-28 09:07:58 +0200

    repalce keen-slider with embla

--- src/components/ui/GalleryCarousel.tsx
+++ src/components/ui/GalleryCarousel.tsx
@@ -1,32 +1,29 @@
-import 'keen-slider/keen-slider.min.css'
-import {
-  createUniqueId,
-  createEffect,
-  createSignal,
-  For,
-  createMemo,
-  onMount,
-  onCleanup,
-} from 'solid-js'
-import KeenSlider, { type KeenSliderInstance } from 'keen-slider'
+import { onCleanup, createSignal, For, createMemo, onMount } from 'solid-js'
+import useEmblaCarousel from 'embla-carousel-solid'
 import { images } from '../../data/carousel-images'
 
-type SlideContent = {
-  images: string[]
+import { CarouselDots } from './CarouselDots'
+import { CAROUSEL_OPTIONS, CAROUSEL_PLUGINS } from './carousel/config'
+
+const imagesPerSlideMap = {
+  mobile: 1,
+  tablet: 2,
+  desktop: 3,
 }
 
-export const Carousel = () => {
-  const slideId = createUniqueId()
-  const [current, setCurrent] = createSignal(0)
+export default () => {
   const [screenSize, setScreenSize] = createSignal<
     'mobile' | 'tablet' | 'desktop'
   >('desktop')
-  let sliderRef: KeenSliderInstance | null = null
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    CAROUSEL_OPTIONS,
+    CAROUSEL_PLUGINS,
+  )
 
   const groupedSlides = createMemo(() => {
-    const imagesPerSlide =
-      screenSize() === 'mobile' ? 1 : screenSize() === 'tablet' ? 2 : 3
-    const slides: SlideContent[] = []
+    const imagesPerSlide = imagesPerSlideMap[screenSize()]
+    const slides = []
 
     for (let i = 0; i < images.length; i += imagesPerSlide) {
       slides.push({
@@ -52,66 +49,41 @@ export const Carousel = () => {
 
     window.addEventListener('resize', handleResize)
     onCleanup(() => window.removeEventListener('resize', handleResize))
-
-    setTimeout(() => {
-      sliderRef = new KeenSlider(`#${slideId}`, {
-        loop: true,
-        slideChanged(s) {
-          setCurrent(s.track.details.rel)
-        },
-      })
-    })
   })
-
-  createEffect(() => {
-    const device = screenSize()
-    setTimeout(() => {
-      if (sliderRef) {
-        sliderRef.update({
-          loop: true,
-          slides: { spacing: device === 'mobile' ? 5 : 16 },
-        })
-      }
-    }, 1000)
-  })
-
-  const handleDotClick = (index: number) => {
-    if (sliderRef) {
-      sliderRef.moveToIdx(index)
-    }
-  }
 
   return (
-    <div class="relative">
-      <div
-        id={slideId}
-        class="keen-slider xl:max-w-[68%] m-auto h-[50vh] sm:h-[65vh] md:h-[70vh] lg:h-[80vh] mb-5 xl:mb-10"
-      >
+    <div ref={emblaRef} class="overflow-x-hidden xl:max-w-[68%] m-auto">
+      <div class="flex touch-pan-y touch-pinch-zoom">
         <For each={groupedSlides()}>
           {(slide) => (
-            <div class="keen-slider__slide flex flex-col h-full">
+            <div
+              classList={{
+                'flex-[0_0_100%] px-3': true,
+                'h-[50vh] sm:h-[65vh] md:h-[70vh] lg:h-[80vh]': true,
+              }}
+            >
               {slide.images.length === 3 ? (
                 <div class="flex flex-col md:flex-row gap-4 h-full">
                   <div class="flex flex-col md:flex-row flex-1 gap-4 h-full">
                     <div class="flex-1 overflow-hidden">
                       <img
                         src={slide.images[0]}
-                        class="w-full h-full object-cover object-center"
+                        class="w-full h-full object-cover"
                         loading="lazy"
                       />
                     </div>
-                    <div class="flex flex-col gap-4 w-full md:w-1/2">
+                    <div class="flex flex-col gap-4 w-full md:w-1/2 h-full">
                       <div class="flex-1 overflow-hidden">
                         <img
                           src={slide.images[1]}
-                          class="w-full h-full object-cover object-center"
+                          class="w-full h-full object-cover"
                           loading="lazy"
                         />
                       </div>
                       <div class="flex-1 overflow-hidden">
                         <img
                           src={slide.images[2]}
-                          class="w-full h-full object-cover object-center"
+                          class="w-full h-full object-cover"
                           loading="lazy"
                         />
                       </div>
@@ -143,20 +115,7 @@ export const Carousel = () => {
           )}
         </For>
       </div>
-
-      <div class="flex justify-center gap-2">
-        <For each={Array.from({ length: groupedSlides().length }, (_, i) => i)}>
-          {(i) => (
-            <button
-              onClick={() => handleDotClick(i)}
-              class={`w-7 h-2 transition-colors ${
-                current() === i ? 'bg-black' : 'bg-gray-400'
-              }`}
-              aria-label={`Go to slide ${i + 1}`}
-            />
-          )}
-        </For>
-      </div>
+      <CarouselDots api={emblaApi} />
     </div>
   )
 }
