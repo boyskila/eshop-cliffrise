48bfa6a47b7fb824ea080617d5a79f07f34ad64d
Boyko Lalov <boiskila@gmail.com>
2026-01-31 22:31:46 +0200

    remove quoted section
    the site already have feedback carousel which is a proof for quality so quoted section is no longer needed

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,12 +2,5 @@ import ProductsSection from './ProductsSection.astro'
 import EventsSection from './EventsSection.astro'
 import AboutSection from './AboutSection.astro'
 import CauseSection from './CauseSection.astro'
-import QuotesSection from './QuotesSection.astro'
 
-export {
-  ProductsSection,
-  EventsSection,
-  AboutSection,
-  CauseSection,
-  QuotesSection,
-}
+export { ProductsSection, EventsSection, AboutSection, CauseSection }
