97c68c5354da152558096069feaf57fedcf0bb15
Boyko Lalov <boiskila@gmail.com>
2026-02-09 01:42:07 +0200

    setup infrastructure for github pages deployment

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,15 @@
+/// <reference types="astro/client" />
+
 declare namespace App {
   interface SessionData {
     cart: import('@actions').CartItem[]
   }
+}
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
