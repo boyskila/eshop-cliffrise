f573c94c5a7e58545f88e0f5ab403325765914ca
Boyko <boyskila@users.noreply.github.com>
2026-02-10 18:27:08 +0200

    Revert "setup infrastructure for github pages deployment"
    This reverts commit 8489909cea70247c646b3986e80b3f6cd30aac7f.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,5 @@
-/// <reference types="astro/client" />
-
 declare namespace App {
   interface SessionData {
     cart: import('@actions').CartItem[]
   }
-}
-
-interface ImportMetaEnv {
-  readonly BASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
 }
