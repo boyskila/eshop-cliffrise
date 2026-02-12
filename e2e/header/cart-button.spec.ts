84c6d9c15ca2b58169822c30e2cb01b25db3db02
Boyko Lalov <boiskila@gmail.com>
2026-02-13 00:38:43 +0200

    adjust port for playwright

--- e2e/header/cart-button.spec.ts
+++ e2e/header/cart-button.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Accessibility', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:4321')
+    await page.goto('/')
   })
   test('cart button is visible and has expected accessibility attributes', async ({
     page,
@@ -52,7 +52,7 @@ test.describe('Accessibility', () => {
 
 test.describe('Functionallity', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:4321')
+    await page.goto('/')
   })
   test('cart button updates cart count when items are added to cart', async ({
     page,
