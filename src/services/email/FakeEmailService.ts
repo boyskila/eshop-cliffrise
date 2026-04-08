6d64256dbf0cdd24cbce547b1ce5e9754cca0580
Boyko Lalov <boiskila@gmail.com>
2026-04-08 09:45:07 +0300

    implement post-payment email notification

--- src/services/email/FakeEmailService.ts
+++ src/services/email/FakeEmailService.ts
@@ -1,10 +1,23 @@
 import type { EmailData, EmailResult, EmailService } from '@types'
 
-export const sentEmails: EmailData[] = []
+export const sentEmails: (EmailData & { html?: string })[] = []
 
 export class FakeEmailService implements EmailService {
   async send(data: EmailData): Promise<EmailResult> {
-    sentEmails.push(data)
+    // Synthesize html from template variables so tests can assert on content
+    if (
+      'template' in data &&
+      data.template?.variables &&
+      !('html' in data && data.html)
+    ) {
+      const vars = data.template.variables
+      const html = Object.values(vars).join(' ')
+      sentEmails.push({ ...data, html } as unknown as EmailData & {
+        html: string
+      })
+    } else {
+      sentEmails.push(data)
+    }
     return { success: true }
   }
 }
