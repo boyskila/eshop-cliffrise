e743abe8a723e2b5b9fa8376fdf0fd9584bf66f7
Boyko Lalov <boiskila@gmail.com>
2026-05-24 21:53:54 +0300

    add founder pages

---
+++ e2e/about-section.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('About section profile links', () => {
+  test('founder names link to dedicated pages', async ({ page }) => {
+    await page.goto('/en/#about-us')
+
+    const aboutSection = page.locator('#about-us')
+    await expect(
+      aboutSection.getByRole('link', { name: 'Boyko Lalov' }),
+    ).toHaveAttribute('href', '/en/people/boyko-lalov/')
+    await expect(
+      aboutSection.getByRole('link', { name: 'Alex Yanev' }),
+    ).toHaveAttribute('href', '/en/people/alex-yanev/')
+  })
+
+  test('clicking a founder name opens the dedicated profile page', async ({
+    page,
+  }) => {
+    await page.goto('/en/#about-us')
+
+    await page
+      .locator('#about-us')
+      .getByRole('link', { name: 'Boyko Lalov' })
+      .click()
+
+    await expect(page).toHaveURL(/\/en\/people\/boyko-lalov\/$/)
+    await expect(
+      page.getByRole('heading', { name: 'Boyko Lalov' }),
+    ).toBeVisible()
+  })
+})
