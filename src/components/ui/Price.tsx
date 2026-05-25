1938ca09bb927300fa6032d8c66a028bd73938b3
Boyko Lalov <boiskila@gmail.com>
2026-05-25 18:11:03 +0300

    make Price component reactive

--- src/components/ui/Price.tsx
+++ src/components/ui/Price.tsx
@@ -1,5 +1,6 @@
 import type { Locale } from '@types'
 import { formatPriceWithBgn } from '@utils/func'
+import { createMemo } from 'solid-js'
 
 type Props = {
   amount: number
@@ -10,22 +11,21 @@ type Props = {
 }
 
 export const Price = (props: Props) => {
-  const [eurPrice, bgnPrice] = formatPriceWithBgn(
-    props.amount,
-    props.lang,
-    props.amountBgn,
+  const prices = createMemo(() =>
+    formatPriceWithBgn(props.amount, props.lang, props.amountBgn),
   )
+
   return (
     <span class={props.class}>
-      {eurPrice}
-      {bgnPrice && (
+      {prices()[0]}
+      {prices()[1] && (
         <span
           classList={{
             'text-gray-500 ml-1 text-base font-normal': true,
             [props.bgnClass ?? '']: true,
           }}
         >
-          / {bgnPrice}
+          / {prices()[1]}
         </span>
       )}
     </span>
