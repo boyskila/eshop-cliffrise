8ed1eb1891005719da96595b0ded250e3e37a3af
Boyko Lalov <boiskila@gmail.com>
2026-06-19 00:07:22 +0300

    improve event section SEO

---
+++ src/data/events.ts
@@ -0,0 +1,9 @@
+export const TETEVEN_CLIMB_2026 = {
+  url: 'https://fb.me/e/7ebntwiFq',
+  startDate: '2026-09-18',
+  endDate: '2026-09-20',
+  location: {
+    name: 'Teteven',
+    addressCountry: 'BG',
+  },
+} as const
