6ec1497171003b1b5b68fa4bb8ebc154513b582d
Boyko Lalov <boiskila@gmail.com>
2026-06-18 23:16:20 +0300

    improve google analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,17 @@
+/// <reference types="astro/client" />
+
 declare namespace App {
   interface SessionData {
     cart: import('@actions').CartItem[]
   }
+}
+
+interface Window {
+  dataLayer?: unknown[]
+  gtag?: (...args: unknown[]) => void
+  __gaConsentInitialized?: boolean
+  __gaLoaded?: boolean
+  __gaLoading?: boolean
+  __gaLastPageView?: string
+  __gaListenersBound?: boolean
 }
